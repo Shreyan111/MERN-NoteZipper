16130ab237b522a2ff78f0b6c317fea26279b331
Shreyan111 <shreyan.sanyal2020@vitstudent.ac.in>
2022-06-11 18:19:30 +0530

    changes in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import LandingPage from './screens/LandingPage/LandingPage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MyNotes from './screens/MyNotes/MyNotes';
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello</p>
-      <p>Hello 123</p>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route exact path='/' element={<LandingPage />} />
+        <Route exact path='/mynotes' element={<MyNotes />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+    Container,
+    Form,
+    FormControl,
+    Nav,
+    Navbar,
+    NavDropdown
+} from "react-bootstrap";
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <Navbar bg="primary" expand="lg" variant='dark'>
+            <Container>
+                <Link to='/'>
+                    <Navbar.Brand>NoteZipper</Navbar.Brand>
+                </Link>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className='m-auto'>
+                        <Form inline>
+                            <FormControl type="text" placeholder='Search' className='mr-sm-2' />
+                        </Form>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="/mynotes">
+                            <Link to='/mynotes'>
+                                My Notes
+                            </Link>
+                        </Nav.Link>
+                        <NavDropdown title="Shreyan Sanyal" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">My Profile</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Logout</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default Header
