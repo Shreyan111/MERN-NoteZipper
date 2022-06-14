4a022a8b8ab7b07b4fe06669057c89a0febdb4d5
Shreyan111 <shreyan.sanyal2020@vitstudent.ac.in>
2022-06-14 17:22:58 +0530

    Preparing for deployment

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const connectDB = require("./config/db");
 const userRoutes = require('./routes/userRoutes');
 const noteRoutes = require('./routes/noteRoutes');
 const { errorHandler, notFound } = require("./middleware/errorMiddleware.js");
+const path = require('path');
 
 const app = express();
 dotenv.config();
@@ -31,6 +32,28 @@ app.use(errorHandler);
 
 //     res.send(note);
 // })
+
+// --------------------------deployment------------------------------
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+    app.get("*", (req, res) =>
+        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    );
+} else {
+    app.get("/", (req, res) => {
+        res.send("API is running..");
+    });
+}
+// --------------------------deployment------------------------------
+
+// Error Handling middlewares
+// app.use(notFound);
+// app.use(errorHandler);
+
+
 
 const PORT = process.env.PORT || 5000;
 
